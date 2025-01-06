83956e6af3e4bd957bd2787d70c3c8d0e8bc9df9
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-06 13:12:21 +0100

    Create While boucle to re-start,  re-fill and re-print  the time value

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Load time module
 import time
 
-# Save the valeur in a variable
-current_time = time.strftime("%H:%M:%S")
-
-# Print the current time
-print(current_time)
+# The boucle While recharge the current_time value, 
+# print and wait one second to re-start the cycle
+while True:
+    # Save the valeur in a variable
+    current_time = time.strftime("%H:%M:%S")
+    # Print the current time
+    print(f"Hora actual: {current_time}")
+    # Wait one second
+    time.sleep(1)
