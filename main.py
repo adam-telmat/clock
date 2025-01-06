e11a68f2f1665b0b981ddd75962e43d8317f6739
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-06 19:08:55 +0100

    Without changes to report

--- main.py
+++ main.py
@@ -2,20 +2,16 @@
 import time
 
 
-
-# The boucle While recharge the current_time value, 
-# print and wait one second to re-start the cycle
-i = 0
-# The hour is showed for ten seconds
-while i < 10:
-    # the iterator increases to 1
-    i += 1
-    # Save the valeur in a variable
-    current_time = time.strftime("%H:%M:%S")
-    # Print the current time
-    print(current_time)
-    # Wait one second
-    time.sleep(1) 
+def clock():
+    # The boucle While recharge the current_time value, 
+    # print and wait one second to re-start the cycle
+    while True:
+        # Save the valeur in a variable
+        current_time = time.strftime("%H:%M:%S")
+        # Print the current time
+        print(current_time)
+        # Wait one second
+        time.sleep(1) 
 
 
 
@@ -28,4 +24,5 @@ def display_time():
     new_time_tuple = (new_hour, new_minute, new_second)
     print(new_time_tuple)
 
+clock()
 display_time()
