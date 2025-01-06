0fb02d257428f5e536c8750baca757e10805a2b3
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-06 14:15:07 +0100

    Add iterator to blucle While (to vies the hour) and first aproximation to change hour function

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # Load time module
 import time
 
+
+
 # The boucle While recharge the current_time value, 
 # print and wait one second to re-start the cycle
-while True:
+i = 0
+# The hour is showed for ten seconds
+while i < 10:
+    # the iterator increases to 1
+    i += 1
     # Save the valeur in a variable
     current_time = time.strftime("%H:%M:%S")
     # Print the current time
     print(current_time)
     # Wait one second
-    time.sleep(1)
+    time.sleep(1) 
+
+
+
+def display_time():
+    print("Change the hour")
+    new_hour = int(input("New hour: "))
+    new_minute = int(input("New minute: "))
+    new_second = int(input("New second : "))
+    
+    new_time_tuple = (new_hour, new_minute, new_second)
+    print(new_time_tuple)
+
+display_time()
