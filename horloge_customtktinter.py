6c8368b11b7bba23bdfd5dd3f1df519804b94685
yannis-messadia <yannis.messadia@laplateforme.io>
2025-01-08 13:41:10 +0100

    add a wip clock

--- horloge_customtktinter.py
+++ horloge_customtktinter.py
@@ -1,4 +1,14 @@
 import customtkinter
+import time
+import datetime
+import threading
 
+customtkinter.set_appearance_mode('dark')
+customtkinter.set_default_color_theme('blue')
+
+# Create the main application window
 app = customtkinter.CTk()
+app.title("L'horlage de mamie")
+app.geometry('420x220')
+
 app.mainloop()
