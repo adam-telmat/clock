1e2532288e36b42d2b73b3fbffb64adc4a89b613
andres-montes-zuluaga <andres.montes-zuluaga@laplateforme.io>
2025-01-10 11:53:21 +0100

    <feat>[config] add two functions for capturing a new time and modifying it
    although it could be done with a single function, it is a requirement to receive a tuple by parameter in the afficher_heure() function, so a function capture_new_time() is created specifically to create the tuple and return the result

--- kiss_clock.py
+++ kiss_clock.py
@@ -2,13 +2,43 @@ import datetime
 import time
 from datetime import timedelta
 
-def actual_time():
+def current_time():
     try:
         while True:
-            present_time = datetime.datetime.now()
-            print(present_time.strftime("%H:%M:%S"))
+            current_time = datetime.datetime.now()
+            print(current_time.strftime("%H:%M:%S"))
             time.sleep(1)
     except KeyboardInterrupt:
         print("\nRetour au menu")
 
-actual_time()
+
+#current_time()
+
+def capture_new_time():
+    h = int(input("Heures (0-23) : "))
+    m = int(input("Minutes (0-59) : "))
+    s = int(input("Secondes (0-59) : "))
+    if 0 <= h < 24 and 0 <= m < 60 and 0 <= s < 60:
+        new_time_tuple = (h, m, s)
+    else:
+        print("Valeurs invalides.")
+    return new_time_tuple
+
+'''print(new_hour_tuple[0])
+print(new_hour_tuple[1])
+print(new_hour_tuple[2])'''
+
+
+def afficher_heure(new_hour_tuple):
+    h = new_hour_tuple[0]
+    m = new_hour_tuple[1]
+    s = new_hour_tuple[2]
+    present_time = datetime.datetime.now()
+    current_time = present_time.replace(hour=h, minute=m, second=s)
+    print("Heure réglée avec succès.")
+    print(current_time.strftime("%H:%M:%S"))
+
+
+
+new_hour_tuple = capture_new_time()
+afficher_heure(new_hour_tuple)
